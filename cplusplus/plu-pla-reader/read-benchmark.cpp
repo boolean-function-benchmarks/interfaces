e55d4a506719197fee3c65e852f9e42f89693d39
Roman Kalkreuth <roman.kalkreuth@tu-dortmund.de>
2022-12-14 23:59:31 +0100

    Documentation added for benchmark-reader.cpp

--- cplusplus/plu-pla-reader/read-benchmark.cpp
+++ cplusplus/plu-pla-reader/read-benchmark.cpp
@@ -1,11 +1,11 @@
 //============================================================================
-// Project     :
+// Project     : General Boolean Function Benchmark Suite
 // Author      : Roman Kalkreuth
 //         		 https://orcid.org/0000-0003-1449-5131,
 //          	 https://www.researchgate.net/profile/Roman-Kalkreuth,
 //         		 https://twitter.com/RomanKalkreuth
 // Version     : 1.0
-// Description :
+// Description : C++ interface for PLU/PLA Boolean function benchmark files.
 // License 	   :
 //============================================================================
 
@@ -17,9 +17,6 @@ int main() {
 
 	reader->read_plu_file("data/add1c.plu");
 	reader->print_compressed_data();
-
-	//reader->read_pla_file("data/add1.pla");
-	//reader->print_truth_table();
 
 	delete reader;
 }
